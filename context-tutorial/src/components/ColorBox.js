65d8fe6f1e31ca3c8bed89f19bbcd688311e88d5
nogomin <nogomindev@gmail.com>
2023-12-01 13:54:41 +0900

    Context API useContext

--- context-tutorial/src/components/ColorBox.js
+++ context-tutorial/src/components/ColorBox.js
@@ -1,6 +1,9 @@
+import { useContext } from "react";
 import ColorContext, { ColorConsumer } from "../contexts/color"
 
 const ColorBox = () => {
+    // useContext는 함수 컴포넌트에서만 사용가능하지만 매우 편리하다.
+    // const {state} = useContext(ColorContext);
     return (
         <ColorConsumer>
             {({state}) => (
