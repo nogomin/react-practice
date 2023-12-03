8af4b91f7e5bb56b8239b2e6b09d8bec94d1d657
nogomin <nogomindev@gmail.com>
2023-12-03 19:23:58 +0900

    Redux Hook
    useSelector, useDispatch, useActions

--- react-redux-tutorial/src/container/CounterContainer.js
+++ react-redux-tutorial/src/container/CounterContainer.js
@@ -1,29 +1,36 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { useCallback } from 'react';
+import { connect, useDispatch, useSelector } from 'react-redux';
 import Counter from '../components/Counter';
 import { increase, decrease } from '../modules/counter';
 
-const CounterContainer = ({number, increase, decrease}) => {
+const CounterContainer = () => {
+    const number = useSelector(state => state.counter.number);
+    const dispatch = useDispatch();
+    // 숫자가 바뀌어서 컴포넌트가 리렌더링될 때마다 onIncrease, onDecrease 함수가 새롭게 만들어지는걸 방지하려면 useCallback
+    const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+    const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
     return (
-        <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+        <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease} />
     )
 }
 
-const mapStateToProps = state => ({
-    number: state.counter.number
-});
-const mapDispatchToProps = dispatch => ({
-    increase: () => {
-        // console.log('increase')
-        dispatch(increase());
-    },
-    decrease: () => {
-        // console.log('decrease')
-        dispatch(decrease());
-    }
-})
+export default CounterContainer;
 
-export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
+// const mapStateToProps = state => ({
+//     number: state.counter.number
+// });
+// const mapDispatchToProps = dispatch => ({
+//     increase: () => {
+//         // console.log('increase')
+//         dispatch(increase());
+//     },
+//     decrease: () => {
+//         // console.log('decrease')
+//         dispatch(decrease());
+//     }
+// })
+
+// export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
 // 간결한 버전. mapDispatchToProps에 함수형태가 아닌 액션 생성함수로 이루어진 객체 형태로 넣어주면 connect함수가 내부적으로 bindActionCreators 작업 대신해줌
 // export default connect(
 //     state => ({
--- react-redux-tutorial/src/container/TodoContainer.js
+++ react-redux-tutorial/src/container/TodoContainer.js
@@ -1,27 +1,45 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import Todos from '../components/Todos';
-import { connect } from 'react-redux';
+import { connect, useDispatch, useSelector } from 'react-redux';
 import { changeInput, insert, toggle, remove } from '../modules/todos';
+import { useActions } from '../lib/useActions';
 
-const TodoContainer = ({input, todos, changeInput, insert, toggle, remove}) => {
+const TodoContainer = () => {
+    const {input, todos} = useSelector(({todos}) => ({
+        input: todos.input,
+        todos: todos.todos
+    }))
+    // const dispatch = useDispatch();
+    // const onChangeInput = useCallback((input) => dispatch(changeInput(input)), [dispatch]);
+    // const onInsert = useCallback(text => dispatch(insert(text)), [dispatch]);
+    // const onToggle = useCallback(id => dispatch(toggle(id)), [dispatch]);
+    // const onRemove = useCallback(id => dispatch(remove(id)), [dispatch]);
+
+    // useAction Hook은 공식 지원은 아니지만 유용한 recipe임. 액션 생성 함수를 디스패치하는 함수로 변환해준다.
+    // useActions(액션생성함수로 이뤄진 배열, dep)
+    const [onChangeInput, onInsert, onToggle, onRemove] = useActions([changeInput, insert, toggle, remove], []);
     return (
         <Todos 
             input={input}
             todos={todos}
-            onChangeInput={changeInput}
-            onInsert={insert}
-            onToggle={toggle}
-            onRemove={remove}
+            onChangeInput={onChangeInput}
+            onInsert={onInsert}
+            onToggle={onToggle}
+            onRemove={onRemove}
         />
     );
 };
+/**
+ * connect함수를 사용했다면 해당 컨테이너 컴포넌트의 props가 바뀌지않으면 리렌더링이 자동으로 방지되지만 useSelector는 그렇지 않다. React.memo로 감싸줘야함
+ */
+export default TodoContainer;
 
-export default connect(({todos}) => ({
-    input: todos.input,
-    todos: todos.todos
-}), {
-    changeInput,
-    insert,
-    toggle,
-    remove
-})(TodoContainer);
+// export default connect(({todos}) => ({
+//     input: todos.input,
+//     todos: todos.todos
+// }), {
+//     changeInput,
+//     insert,
+//     toggle,
+//     remove
+// })(TodoContainer);
